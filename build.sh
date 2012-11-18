3eaa8c1b19db51be7c3ce4b3d2bf877bc00128ca
UEHARA Junji <uehaj@jggug.org>
2012-11-18 10:55:14 +0900

    move generating jar to build/lib

--- build.sh
+++ build.sh
@@ -5,8 +5,9 @@ if [ -d ./build ]; then
 fi
 mkdir build
 mkdir build/classes
+mkdir build/lib
 
 groovyc -d build/classes src/main/staticalizer/TypeLogger.groovy src/main/staticalizer/transform/TypeLoggingASTTransformation.groovy src/main/staticalizer/transform/WithTypeLogging.groovy
 
-jar cf staticalizer.jar -C build/classes .
+jar cf build/lib/staticalizer.jar -C build/classes .
 
