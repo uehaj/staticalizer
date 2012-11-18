e2bc316c6fbaa2cf48eabc5e0589270d7027fc40
UEHARA Junji <uehaj@jggug.org>
2012-11-18 10:59:07 +0900

    change to commit original(not be patched) sample*.groovy file

--- resources/test/sample1.groovy
+++ resources/test/sample1.groovy
@@ -1,8 +1,6 @@
 import staticalizer.transform.WithTypeLogging
 @WithTypeLogging
 def greet(a,b,c,d) {
-//greet(java.lang.Integer a,java.math.BigDecimal b,java.lang.String c,java.util.ArrayList d)
-//greet(java.math.BigDecimal a,java.math.BigDecimal b,java.lang.Integer c,java.util.LinkedHashMap d)
   println "Hello World"
 }
 
--- resources/test/sample2.groovy
+++ resources/test/sample2.groovy
@@ -1,14 +1,11 @@
 import staticalizer.transform.WithTypeLogging
 @WithTypeLogging
 def greet(a,b,c,d) {
-//greet(java.lang.Integer a,java.math.BigDecimal b,java.lang.String c,java.util.ArrayList d)
-//greet(java.math.BigDecimal a,java.math.BigDecimal b,java.lang.Integer c,java.util.ArrayList d)
   println "Hello World"
 }
 
 @WithTypeLogging
 def greet2(a,b,c,d) {
-//greet2(java.math.BigDecimal a,java.math.BigDecimal b,java.lang.Integer c,java.util.ArrayList d)
   println "Hello World"
 }
 
